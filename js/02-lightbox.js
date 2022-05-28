408182d90ea32b9f622d85f61521155bd118ba83
ts4ed <103256726+ts4ed@users.noreply.github.com>
2022-05-28 19:14:11 +0300

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const newGallery = gallery(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", newGallery);
+galleryEl.addEventListener("click", onClickEvent);
+
+function gallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"  />
+</a>`;
+    })
+    .join("");
+}
+function onClickEvent(elm) {
+  elm.preventDefault();
+}
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});

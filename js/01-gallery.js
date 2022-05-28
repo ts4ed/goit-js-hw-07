7ace6c59979c1e356853478dd2af959082b26770
ts4ed <103256726+ts4ed@users.noreply.github.com>
2022-05-28 16:26:15 +0300

    Update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const newGallery = gallery(galleryItems);
+const base = document.querySelector("body")
+window.addEventListener("keydown", esOnEvent);
+
+galleryEl.insertAdjacentHTML("beforeend", newGallery);
+galleryEl.addEventListener("click", onClickEvent);
+
+function gallery(galleryItems) {
+  return galleryItems.map(({ preview, original, descrption }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link"  href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${descrption}"
+    />
+  </a>
+</div>`;
+  }).join("");
+}
+
+// preventDefault();
+
+function onClickEvent(elm) {
+  if (!elm.target.classlist.contains("gallery__image")) {
+    return;
+  }
+
+  for (let i = 0; i < galleryItems.length; i += 1) {
+    const el = galleryItems[i];
+    if (elm.target.src === el.preview) {
+      const instance = basicLightbox.create(`
+    <img src="${el.original}" width="800" height="600" onclick="return false" >
+`);
+      
+      instance.show();
+      
+    }
+    
+  }
+  
+}
+function esOnEvent() {
+  if (event.code === "Escape") {
+   console.log(`esc`);
+  }
+}
+console.log(base.lastElementChild);
+
+
+// решить с переходом на новую страницу при открытии изображения 
+// при нажатии Escape  удаляется клас div с "плейлистом" через (textContent? )
